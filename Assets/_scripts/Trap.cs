6a174990019e87d0325843613334965fb140baa9
Andrew Bradley <cspotcode@gmail.com>
2020-06-28 13:19:03 -0400

    comments

--- Assets/_scripts/Trap.cs
+++ Assets/_scripts/Trap.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// If there is anything unique about a trap, it can be stored as fields here.
+/// Based on our discussion, traps are completely identical, even to the point that you
+/// can set off your own trap, so traps don't need any sort of "owner" or identity.
+/// So this class is currently empty and unused.
 public class Trap : MonoBehaviour
 {
     public TrapType type;
