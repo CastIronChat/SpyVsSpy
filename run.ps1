33ba72d215503685ad8564d9c5aa2668583c0eb4
Andrew Bradley <cspotcode@gmail.com>
2020-07-03 20:07:10 -0400

    WIP dev scripts to build webgl, launch locally, and publish to github

--- run.ps1
+++ run.ps1
@@ -14,13 +14,47 @@ To run a subcommand, for example "format":
 Subcommands are declared in the switch() statement below.
 #>
 
-Write-Host "Running command $Command..."
-switch($Command) {
-    "format" {
-        dotnet tool run dotnet-format .\Assembly-CSharp.csproj
-        dotnet tool run dotnet-format .\Assembly-CSharp-Editor.csproj
-    }
-    default {
-        Write-Error "Unexpected command: $Command"
+function run() {
+    switch($Command) {
+        "format" {
+            dotnet tool run dotnet-format .\Assembly-CSharp.csproj
+            dotnet tool run dotnet-format .\Assembly-CSharp-Editor.csproj
+        }
+        "build-webgl" {
+            # THIS SCRIPT IS NOT WORKING
+            # I don't understand -batchmode so I don't know why it fails.
+            # I'll come back to this.
+            FindUnity
+            # try to locate the Unity executable
+            Start-Process -Wait -FilePath $Unity -ArgumentList @(
+                '-projectPath', $PSScriptRoot, '-quit', '-batchmode', '-executeMethod', 'WebGLBuilder.build'
+            )
+        }
+        "serve-webgl" {
+            # Launch the browser and serve webgl build with a little webserver
+            Start "http://127.0.0.1:4507"
+            & "$PSScriptRoot/scripts/deno.exe" run --allow-net --allow-read https://deno.land/std/http/file_server.ts ./Builds/WebGL
+        }
+        "publish-webgl" {
+            # TODO push to github pages
+        }
+        default {
+            Write-Error "Unexpected command: $Command"
+        }
     }
 }
+
+$Unity = $null
+function FindUnity() {
+    $candidates = @(
+        "C:\Program Files\Unity\Hub\Editor\2019.2.19f1\Editor\Unity.exe",
+        "<PATH TO OTHER UNITY INSTALLATION>"
+    )
+    $candidate = $candidates | ? {test-path $_} | select-object -first 1
+    if($candidate -eq $null) {
+        write-host 'Unable to locate Unity executable.  Add your install to the list of paths in "run.ps1" and try again.'
+    }
+    ([ref]$Unity).Value = $candidate
+}
+
+run
