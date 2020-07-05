041b4c21d1fd136dc52d94ef5f0cc4593b95bdfa
Andrew Bradley <cspotcode@gmail.com>
2020-07-05 18:28:49 -0400

    Add Editor window to enable single-scene playing when multi-scene editing

---
+++ Assets/_scripts/Utilities/Editor/MultiSceneEditingHelper.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace MultiSceneEditingHelper
+{
+    public class MultiSceneEditingWindow : EditorWindow
+    {
+        [MenuItem( "Window/Multi-Scene Editing Helper" )]
+        public static void ShowWindow()
+        {
+            GetWindow<MultiSceneEditingWindow>();
+        }
+        public MultiSceneEditingWindow()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+        }
+        void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if ( DisableAllButActiveScene )
+            {
+                if ( state == PlayModeStateChange.ExitingEditMode )
+                {
+                    // Disable all but the active scene
+                    var active = EditorSceneManager.GetActiveScene();
+                    if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
+                    for ( var i = 0; i < EditorSceneManager.sceneCount; i++ )
+                    {
+                        var scene = EditorSceneManager.GetSceneAt( i );
+                        if ( scene.isLoaded && scene != active )
+                        {
+                            closedScenePaths.Add( scene.path );
+                            EditorSceneManager.CloseScene( scene, false );
+                        }
+                    }
+                    }
+                }
+                if ( state == PlayModeStateChange.EnteredEditMode )
+                {
+                    // Re-enable closed scenes
+                    foreach ( var scenePath in closedScenePaths )
+                    {
+                        EditorSceneManager.OpenScene( scenePath, OpenSceneMode.Additive );
+                    }
+                    closedScenePaths.Clear();
+                }
+            }
+        }
+        public bool DisableAllButActiveScene = false;
+        public List<string> closedScenePaths = new List<string>();
+        void OnGUI()
+        {
+            GUILayout.Label( "Extra editor options for multi-scene editing.");
+            GUILayout.Space( 10 );
+            GUILayout.Label( "Check this box if you want \"Play\" mode to only run the active scene when using multi-scene editing." );
+            GUILayout.Label( "When you click Play, all other scenes will be unloaded." );
+            GUILayout.Label( "When you return to Edit mode, the will be re-loaded.");
+
+            DisableAllButActiveScene = EditorGUILayout.Toggle( DisableAllButActiveScene );
+        }
+    }
+}
