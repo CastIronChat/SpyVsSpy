17dbfd2905a2110b7ea4698745819dd4ea314588
Andrew Bradley <cspotcode@gmail.com>
2020-07-05 18:27:26 -0400

    remove my assert class cuz I found Unity's built-in one

--- Assets/_scripts/Utilities/Assert.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public static class Assert
-{
-    public static void NotNull(object v)
-    {
-        if ( v == null ) { throw new Exception( "value cannot be null" ); }
-    }
-}
