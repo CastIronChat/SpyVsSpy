60d874437ac2157d8d640d12eb65d0bf8a20d75f
Andrew Bradley <cspotcode@gmail.com>
2020-07-06 16:13:17 -0400

    Fix build by fixing asmdefs, removing ones I can't quickly get working, and rearranging code to exclude editor-only code from builds

--- Assets/_scripts/Utilities/Description.cs
+++ Assets/_scripts/Utilities/Description.cs
@@ -1,29 +1,13 @@
 using UnityEngine;
-using UnityEditor;
-using System.Text.RegularExpressions;
 
 public class DescriptionAttribute : PropertyAttribute
 {
     public DescriptionAttribute(string description) {
         this.description = Outdent.trim(description);
-        type = MessageType.Info;
+        // TODO disabled cuz I'm not sure I can safely wrap this in #if UNITY_EDITOR or if it'll break serialization.
+        //type = MessageType.Info;
     }
     public readonly string description;
-    public readonly MessageType type;
+    //public readonly MessageType type;
 }
 
-[CustomPropertyDrawer( typeof( DescriptionAttribute ) )]
-class DescriptionDrawer : DecoratorDrawer
-{
-    private DescriptionAttribute descriptionAttribute { get => (DescriptionAttribute)attribute; }
-
-    public override float GetHeight() {
-        var content = new GUIContent(descriptionAttribute.description);
-        return EditorStyles.helpBox.CalcHeight(content, EditorGUIUtility.currentViewWidth - 52);
-    }
-
-    public override void OnGUI(Rect position)
-    {
-        EditorGUI.HelpBox(position, descriptionAttribute.description.Trim(), descriptionAttribute.type);
-    }
-}
