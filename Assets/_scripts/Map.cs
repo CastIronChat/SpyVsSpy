52c6a2699f2370ba6679ef1cd725b84c276cda43
Andrew Bradley <cspotcode@gmail.com>
2020-07-11 18:03:55 -0400

    Add "Map" component to be placed on root of the map and encapsulate map-specific properties, expose accessors for lists of map elements

---
+++ Assets/_scripts/Map.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+/// Placed on the root GameObject of the Map.  This hierarchy must contain all map elements: hiding places, doors, etc.
+/// But they can exist in any arrangement. (deeply nested under child objects or not)
+/// </summary>
+public class Map : MonoBehaviour
+{
+    public RectTransformUtility roomSizeReference;
+
+    /// <summary>
+    /// At the moment, a silly abstraction, since we're just wrapping GetComponentsInChildren<>
+    /// Maybe it will need to do extra filtering in the future; I'm not sure.
+    /// </summary>
+    public T[] getComponents<T>() where T : Component
+    {
+        return GetComponentsInChildren<T>();
+    }
+
+    private void OnValidate()
+    {
+        Assert.IsNotNull( roomSizeReference, "Map must have a reference to its roomSizeReference so that cameras know how to snap to rooms." );
+    }
+}
